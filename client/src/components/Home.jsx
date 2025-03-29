6000aee616b3fca69a48c20a55e1793326023319
yasindu2000 <yasindudahanayake@gmail.com>
2025-03-29 10:16:46 +0530

    complete home page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import '../CSS/Home.css'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className="hero">
+      <div className="hero-content">
+        <h1 className="hero-text">Book Shop</h1>
+        <p className="hero-description">
+          Browse the collection of our best top interresting Books.
+          you will definitely find what you are looking for.
+        </p>
+      </div>
+
+      <div className="hero-image"></div>
+    </div>
   )
 }
 
