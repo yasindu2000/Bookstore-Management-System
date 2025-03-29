c6856f79a2b7cfb209d5ad02b25061e86b378645
yasindu2000 <yasindudahanayake@gmail.com>
2025-03-29 10:51:31 +0530

    complte login page

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import '../CSS/Login.css'
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className="login-page">
+      <div className="login-container">
+        <h2 className="">Login</h2><br/>
+
+        <div className="form-group">
+          <label htmlFor='username'>Username:</label>
+          <input type="text" className="" placeholder='Enter Username' />
+        </div>
+        <div className="form-group">
+          <label htmlFor='username'>Password:</label>
+          <input type="password" className="" placeholder='Enter Password' />
+        </div>
+        <div className="form-group">
+          <label htmlFor='role'>Role:</label>
+          <select name='role' id='role'>
+            <option value="admin">Admin</option>
+            <option value="student">Student</option>
+
+          </select>
+        </div>
+        <button className="btn-login">Login</button>
+      </div>
+    </div>
   )
 }
 
