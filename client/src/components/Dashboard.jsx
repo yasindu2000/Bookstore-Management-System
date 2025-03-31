a115cf1cf9fb00a63f1141ce6bea29e54b9444e2
yasindu2000 <yasindudahanayake@gmail.com>
2025-03-31 15:39:10 +0530

    implement dashboard

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import '../CSS/Dashboard.css'
 
 function Dashboard() {
   return (
-    <div>dashboard</div>
+    <div className="dashboard">
+      <div className="dashboard-box">
+        <h2>Total Books</h2> <br/>
+        <h2></h2>
+      </div>
+
+      <div className="dashboard-box">
+        <h2>Total Students</h2> <br/>
+        <h2></h2>
+      </div>
+
+      <div className="dashboard-box">
+        <h2>Total Admins</h2> <br/>
+        <h2></h2>
+      </div>
+    </div>
   )
 }
 
